d967c8993d1b04834c66db1ac8d703ec75b74925
Vladislav Andrš <vladislav.andrs@gmail.com>
2018-09-18 16:40:04 +0200

    c-form-template-box works, before using functions

--- app/components/c-form-template-box.js
+++ app/components/c-form-template-box.js
@@ -14,38 +14,25 @@ import { scheduleOnce } from '@ember/runloop';
 export default Component.extend({
 
   classNames: ['template-box'],
-  // findInput : false,
   templates : [
     'aaa', 'abc', 'bbbbbb', 'cccc'
   ],
 
-  // After list content showed (Ctrl + T), select input (only once)
-  // didRender() {
-  //   this._super(...arguments);
-  //   if (this.get('findInput')) {
-  //     this.$().find('.template-box-content input').focus().select();
-  //     this.set('findInput', false);
-  //   }
-  // },
-
   init() {
     this._super(...arguments);
-
     // Define computed property dynamically.
     // It is not possible to define computed property with variable in it.
     // Disable save button, when text area is empty.
     defineProperty(this, 'saveDisabled',
-    computed('model.' + this.get('field'), 'templates.[]', function(){
+      computed('model.' + this.get('field'), 'templates.[]', function(){
         var templates = this.get('templates');
         var fieldName = this.get('field');
         var model = this.get('model');
         var text = model.get(fieldName);
         // Text not empty and not same as already saved? Then enable save.
         if (text && !templates.includes(text)) {
-          console.log('saveDisabled==false');
           return false
         } else {
-          console.log('saveDisabled==true');
           return true
         }
       })
@@ -53,31 +40,45 @@ export default Component.extend({
 
   },
 
-  // Ctrl + P : open the template list
+  // setText(text) {
+  //   var fieldName = this.get('field');
+  //   var model = this.get('model');
+  //   var text = model.get(fieldName);
+  //     console.log('setText', text);
+  //     this.get('model').set(fieldName, text);
+  // },
+  //
+  // appendText(text) {
+  //
+  // },
+
   keyDown(event) {
     console.log(event.keyCode);
     console.log(event.ctrlKey);
     console.log('searchTemplate', this.get('searchTemplate'));
-    // console.log('model', this.get('model'));
+
+    // Ctrl + P : open the template list and focus the input
     if (event.ctrlKey == true && event.keyCode == 80) {
       console.log('spuštěno Ctrl+P, focus!');
-      this.set('findInput', true);
-
-      // debugger;
-      // this.toggleProperty('listOpen');
       this.set('listOpen', true);
-      // run look afterRender(() => { jQuery stuff })
+      // wait until render finished, then focus input
       scheduleOnce('afterRender', this, function() {
         this.$().find('.template-box-content input').focus().select();
       });
       return false;
     }
-    // user wrote and pressed ENTER => put first matching template to the textarea
+
+    // user wrote something and pressed ENTER => put/append first matching template to the textarea
     if (this.get('searchTemplate') && event.keyCode == 13) {
-      // debugger;
       var fistFoundTemplate = this.get('rowsFiltered')[0];
+      var model = this.get('model');
       var fieldName = this.get('field');
-      this.get('model').set(fieldName, fistFoundTemplate);
+      var text = model.get(fieldName);
+      if (event.ctrlKey == true) {   // Ctrl + Enter = append template
+        this.get('model').set(fieldName, text + ' ' + fistFoundTemplate);
+      } else {                       // Enter = put template
+        this.get('model').set(fieldName, fistFoundTemplate);
+      }
     }
 
   },
@@ -119,12 +120,11 @@ export default Component.extend({
     applyTemplate(template, event) {
       var fieldName = this.get('field');
       var model = this.get('model');
-      // var text = model[fieldName];
       var text = model.get(fieldName);
       if (event.ctrlKey == true) {
         // Ctlr button pressed => add template to actual text
-        console.log('CTRL, přidat k textu', text + template);
-        this.get('model').set(fieldName, text + template);
+        console.log('CTRL, přidat k textu', text + ' ' + template);
+        this.get('model').set(fieldName, text + ' ' + template);
       } else {
         // without Ctrl => put template to the textarea
         this.get('model').set(fieldName, template);

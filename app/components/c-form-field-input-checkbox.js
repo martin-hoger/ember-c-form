edbdf28d054c4bdce1d652a2c7d786fbcaab0dce
Vladislav Andrš <vladislav.andrs@gmail.com>
2018-10-26 13:28:46 +0200

    checkbox - documentation copy-to-clipboard - 1st version

--- app/components/c-form-field-input-checkbox.js
+++ app/components/c-form-field-input-checkbox.js
@@ -1,33 +1,39 @@
+/*
+Usage: (onChange - fire external action)
+  {{c-form-field-input-checkbox
+    model=this
+    field=selected
+    onChange=(action "select")
+  }}
+*/
+
 import Ember from 'ember';
 import FieldMixin from './c-form-field-mixin';
 
 export default Ember.Component.extend(FieldMixin, {
   fieldType  : 'input-checkbox',
-  valueOn    : 1, 
-  valueOff   : 0, 
+  valueOn    : 1,
+  valueOff   : 0,
 
   //If checked set proper icon.
   isChecked : Ember.computed('model', function () {
     let value = this.get('model.' + this.get('field'));
-    return !(value === false || value === undefined || value === null | value === 0 || value === '0' || value === '' || value === this.get('valueOff')); 
+    return !(value === false || value === undefined || value === null | value === 0 || value === '0' || value === '' || value === this.get('valueOff'));
   }),
 
   value: Ember.computed('isChecked', function () {
-    return this.get('isChecked') ? this.get("valueOn") : this.get("valueOff");
+    return this.get('isChecked') ? this.get('valueOn') : this.get('valueOff');
   }),
 
   actions: {
     toggle: function() {
       this.set('isChecked', !this.get('isChecked'));
-      this.set('model.' + this.get('field'), this.get("value"));
+      this.set('model.' + this.get('field'), this.get('value'));
       // Fire external action passed to this component.
-      if (this.get("onChange")) {
-        this.attrs.onChange(this.get("value"));
+      if (this.get('onChange')) {
+        this.get('onChange')(this.get('value'));
       }
     },
   }
 
 });
-
-
-
--- app/components/c-form-field-textarea-copy-to-clipboard.js
+++ app/components/c-form-field-textarea-copy-to-clipboard.js
@@ -1,12 +1,11 @@
 import Ember from 'ember';
-import moment from 'moment';
 import FieldMixin from './c-form-field-mixin';
 
 export default Ember.Component.extend(FieldMixin, {
   icon       : 'copy',
   buttonText : 'Copy to clipboard',
-  classNames: ['textarea-copy-to-clipboard'],
-
+  fieldType  : 'input-textarea',
+  classNames : ['textarea-copy-to-clipboard'],
 
   actions: {
 

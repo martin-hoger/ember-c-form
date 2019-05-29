a533fdcb30b87baf581c7aeff01a69403716c42f
Lukáš Höger <lukas.hoger@gmail.com>
2019-05-29 22:54:47 +0200

    Added save notification.

--- app/components/c-form-field-input-text.js
+++ app/components/c-form-field-input-text.js
@@ -1,13 +1,26 @@
 import Ember from 'ember';
 import FieldMixin from './c-form-field-mixin';
+import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Component.extend(FieldMixin, {
   // Input type: text, password, etc...
-  type              : 'text',
-  fieldType         : 'input-text',
-  classNameBindings : ['unit:has-unit:', 'suffixIcon:suffix-icon:'],
+  type                       : 'text',
+  fieldType                  : 'input-text',
+  classNameBindings          : ['unit:has-unit:', 'suffixIcon:suffix-icon:'],
+  displayNotificationTimeout : 1500,
 
   keyUp : function() {
     this.sendAction();
-  }
+    if (this.get('saveNotificationTimeout')) {
+      this.get('notification').perform();
+    }
+  },
+  
+  //Display notification.
+  notification: task(function * () {
+    yield timeout(this.get('saveNotificationTimeout'));
+    this.$('.display-notification').transition('scale', '500ms');
+    yield timeout(this.get('displayNotificationTimeout'));
+    this.$('.display-notification').transition('scale', '500ms');
+  }).restartable(),
 });
--- app/components/c-form-field-input-textarea.js
+++ app/components/c-form-field-input-textarea.js
@@ -1,10 +1,23 @@
 import Ember from 'ember';
 import FieldMixin from './c-form-field-mixin';
+import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Component.extend(FieldMixin, {
-  fieldType  : 'input-textarea',
+  fieldType                  : 'input-textarea',
+  displayNotificationTimeout : 1500,
 
   keyPress: function() {
     this.sendAction();
+    if (this.get('saveNotificationTimeout')) {
+      this.get('notification').perform();
+    }
   },
+  
+  //Display notification.
+  notification: task(function * () {
+    yield timeout(this.get('saveNotificationTimeout'));
+    this.$('.display-notification').transition('scale', '500ms');
+    yield timeout(this.get('displayNotificationTimeout'));
+    this.$('.display-notification').transition('scale', '500ms');
+  }).restartable(),
 });

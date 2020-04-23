55d031a40e20e21dc4aefc742d7956f47f3f3db9
Lukáš Höger <lukas.hoger@gmail.com>
2020-04-23 13:55:19 +0200

    Modified form fieldset.

--- app/components/c-form-fieldset.js
+++ app/components/c-form-fieldset.js
@@ -1,17 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames : "form-fieldset",
+  classNames  : "form-fieldset",
+  fixExpanded : false,
+
+  init() {
+    this._super(...arguments);
+    if (this.get('fixExpanded')) {
+      this.set('expanded', true);
+    }
+    if (this.get('fixCollapsed')) {
+      
+      this.set('expanded', false);
+    }
+  },
 
   click: function() {
     this.sendAction();
   },
+
   actions: {
     changeExpanding: function() {
-      if (this.get('expanded') == true) {
-        this.set('expanded', false);
-      } else {
-        this.set('expanded', true);
+      //If fieldset change expand is allowed.
+      if ((!this.get('fixExpanded')) && (!this.get('fixCollapsed'))) {
+        if (this.get('expanded')) {
+          this.set('expanded', false);
+        } else {
+          this.set('expanded', true);
+        }
       }
     }
   }

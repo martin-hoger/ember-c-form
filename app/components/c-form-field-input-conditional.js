84823f7bf9be150e9757b27fac12c3496098877b
Vladislav Andrš <vladislav.andrs@gmail.com>
2020-12-08 15:19:04 +0100

    {{c-form-field-input-conditional}} WIP

--- app/components/c-form-field-input-conditional.js
+++ app/components/c-form-field-input-conditional.js
@@ -1,60 +1,58 @@
+/*
+  Component with 'YES' and 'NO' icons. To set 'value' to specified value or leave it
+  unset. Use it, if you need to know, if the value was set.
+  Not set - buttons gray, no other field. After clicking YES, the {{yield}} is showen
+  to input the value.
+
+  model.field value is:
+  null : value not yet set. {{yield}} field not yet showen.
+  ''   : value set, but not yet to the right value. {{yield}} field showen
+  -1   : value set to 'NO' (e.g. there is NO garden!)
+  something else : set to specified value
+*/
+
 import Component from '@ember/component';
 import FieldMixin from './c-form-field-mixin';
 import { computed, defineProperty } from '@ember/object';
-// import { defineProperty } from '@ember/object';
 
 export default Component.extend(FieldMixin, {
   // Input type: text, password, etc...
   type              : 'text',
   fieldType         : 'input-conditional',
-  classNameBindings : ['unit:has-unit:', 'suffixIcon:suffix-icon:'],
-  // value             : null,
 
   init() {
     this._super(...arguments);
-    // fileId: define computed property dynamically.
+    // value: define computed property dynamically.
     // We need to know the value of this.get('model').get(this.get('field'))
     defineProperty(this, 'value',
       computed('model', 'field', 'model.' + this.get('field'), function () {
         var model = this.get('model');
         var field = this.get('field');
-        console.log('value comp', model.get(field));
         return model.get(field);
       })
     );
   },
 
-  // // (not-eq value "")
+  // YES button clicked or value already set.
+  // value is: '', '12' or other value:
   checkedYes: computed('value', function() {
-    console.log('val', this.get('value'));
-    // a tady ještě zjistit, zda checkedNo nebo má value hodnotu jinou, než null a -1. any(function(object){
-    // Kdyý má totiž valkue == 200, tak musí být zakliknuto checkedYes! Ale když value je null, tak
-    // nezaškrtnuto nic
     return this.get('value') === '' || (this.get('value') !== null && !this.get('checkedNo')) ;
   }),
 
-  // {{#if (not-eq value -1)}}
+  // NO button clicked
   checkedNo: computed('value', function() {
-    console.log('val', this.get('value'));
     return this.get('value') === -1;
   }),
 
-  // isExpanded: computed('value', function() {
-  //   return this.get('checkedYes');
-  //   // return (!this.get('checkedNo') || this.get('value') === null) && (this.get('checkedYes') || this.get('value')) ;
-  // }),
-
-
   actions: {
 
+    // Clicked YES or NO button:
     buttonClick (param) {
       // If value already set, don't overwite it with ''. Finish!
       if (this.get('checkedYes') && param === '') {
         return;
       }
       var modelField = 'model.' + this.get('field');
-      console.log(param);
-      console.log('modelField', modelField);
       // Set model.field to '' or -1
       this.set(modelField, param);
     }

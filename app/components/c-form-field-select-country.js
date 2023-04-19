aa72dfb0dbde746a3bb2c467a37e636680c5a4f6
Vladislav Andrš <vladislav.andrs@gmail.com>
2023-04-19 17:21:34 +0200

    c-form-field-select-country - search icon added

--- app/components/c-form-field-select-country.js
+++ app/components/c-form-field-select-country.js
@@ -9,6 +9,15 @@ import { getNames } from 'ember-i18n-iso-countries';
       label="label to be displayed above"
   }}
 
+  Use when enable searching in countries: searchField="value"
+  {{c-form-field-select-country
+    model=client
+    field="country"
+    label="Občanství"
+    placeholder="Země"
+    searchField="value"
+  }}
+
 */
 export default Ember.Component.extend({
 

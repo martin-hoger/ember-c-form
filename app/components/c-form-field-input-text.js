d48859151bd707306546bc8c1c57c5d5bbbf6ce1
Vladislav Andrš <vladislav.andrs@gmail.com>
2023-09-26 12:44:37 +0200

    c-form-field-input-text: passing class to input possible

--- app/components/c-form-field-input-text.js
+++ app/components/c-form-field-input-text.js
@@ -1,7 +1,12 @@
 /*
     {{c-form-field-input-text}}
     required: when using label, it is a star after label text.
-              If no label, it is a star in the input box. 
+              If no label, it is a star in the input box.
+
+    Inputs:
+      title - title of the <input>
+      inputClassName - you can pass class name to the <input>
+
 */
 
 import Ember from 'ember';
